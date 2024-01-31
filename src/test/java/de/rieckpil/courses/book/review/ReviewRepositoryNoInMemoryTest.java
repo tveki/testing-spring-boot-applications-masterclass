42f2a250b15ef4934bc6f98d7c1783dc47c64ab5
Takács Viktor <takacs.viktor@webstar.hu>
2024-01-31 20:52:13 +0100

    ReviewRepositoryNoInMemoryTest

--- src/test/java/de/rieckpil/courses/book/review/ReviewRepositoryNoInMemoryTest.java
+++ src/test/java/de/rieckpil/courses/book/review/ReviewRepositoryNoInMemoryTest.java
@@ -11,10 +11,19 @@ import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @DataJpaTest
 @Testcontainers(disabledWithoutDocker = true)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class ReviewRepositoryNoInMemoryTest {
+
+  @Autowired
+  private ReviewRepository cut;
 
   @Container
   static PostgreSQLContainer<?> container = new PostgreSQLContainer<>("postgres:12.3")
@@ -25,16 +34,34 @@ class ReviewRepositoryNoInMemoryTest {
   @DynamicPropertySource
   static void properties(DynamicPropertyRegistry registry) {
     registry.add("spring.datasource.url", container::getJdbcUrl);
-    registry.add("spring.datasource.password", container::getPassword);
     registry.add("spring.datasource.username", container::getUsername);
+    registry.add("spring.datasource.password", container::getPassword);
   }
 
-  @Autowired
-  private ReviewRepository cut;
+  @Test
+  void notNull() throws SQLException {
+    assertNotNull(cut);
+  }
 
   @Test
   @Sql(scripts = "/scripts/INIT_REVIEW_EACH_BOOK.sql")
   void shouldGetTwoReviewStatisticsWhenDatabaseContainsTwoBooksWithReview() {
+
+    assertEquals(3, cut.count());
+
+    final List<ReviewStatistic> stats = cut.getReviewStatistics();
+
+    assertEquals(2, stats.size());
+
+    ReviewStatistic stat1 = stats.get(0);
+
+    assertEquals("1234567891234", stat1.getIsbn());
+    assertEquals(1, stat1.getRatings());
+
+    ReviewStatistic stat2 = stats.get(1);
+
+    assertEquals("1234567891235", stat2.getIsbn());
+    assertEquals(2, stat2.getRatings());
   }
 
   @Test
